9cbed77c4e03263589d71e7fa80a1cfbac752767
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-07-01 11:27:46 +0200

    added cypress tests

---
+++ client/cypress/e2e/e2e.cy.js
@@ -0,0 +1,8 @@
+describe('user should be able to manage profiles', () => {
+
+  it('user should be able to log in', () => {
+    // eslint-disable-next-line no-undef
+    cy.visit('http://localhost:3000')
+
+  })
+})
