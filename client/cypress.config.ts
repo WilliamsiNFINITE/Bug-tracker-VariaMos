eec2918bf06d71e49142079c2e11413e707983f2
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-07-15 10:51:12 +0200

    update cypress config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    pageLoadTimeout : 15000,
+    pageLoadTimeout : 10000,
     viewportHeight: 1060,
     projectId: "87jdpk",
   },
